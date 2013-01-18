f5229f6b94f70a4dabfa6306eec85d2484d425f2
Samuel Rounce <me@samuelrounce.co.uk>
2013-01-18 20:10:57 +0000

    Chugging along, still feel rough. Added context classes. Now to initialize controllers & co.

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 var fs = require('fs')
   , path = require('path')
+  , util = require('util')
+  , EventEmitter = require('events').EventEmitter
+  , libutil = require('./lib/util')
   , _;
 
-var validFormats = ['js', 'coffee'];
+var validFormats = ['js', 'coffee']
+  , LoaderContext = require('./lib/loadercontext');
 
 module.exports = Skipper
 
 function Skipper()
 {
-
+  this._controllers = {};
 }
+
+util.inherits(Skipper, EventEmitter);
 
 Skipper.prototype.register = function register( controllerPath, callback )
 {
@@ -19,16 +25,35 @@ Skipper.prototype.register = function register( controllerPath, callback )
 
   controllers = controllers.filter(function( controller ) {
     return validFormats.indexOf( path.extname(controller).replace('.','') ) >= 0;
-  });
+  }.bind(this));
 
   controllers.forEach(function( controller, index ) {
-    console.log(controller, index);
     console.log(controller.replace(new RegExp("\.("+validFormats.join('|')+")$", "gi"), ''));
 
-    var code = require('fs').readFileSync( path.join('./controllers/', controller) );
-    var ldr = new Function('context', 'with (context) {\n' + code + '\n }');
-//  ldr(context);
-  });
+    var source = require('fs').readFileSync( path.join('./controllers/', controller) )
+      , loader = new Function('context', 'with (context) {\n' + source + '\n }')
+      , ctx = new LoaderContext(this);
+
+    loader(ctx);
+
+  }.bind(this));
 }
 
+Skipper.prototype.registerController = function registerController( controller )
+{
+  var ctrlrName = controller.name;
+
+  this._controllers[ctrlrName] = controller;
+}
+
+Skipper.prototype.controller = function controller( name )
+{
+  which = ( this._controllers && this._controllers[ name ] )
+    ? this._controllers[ name ]
+    : false;
+  
+  if(which) {
+    return which;
+  }
+}
 
